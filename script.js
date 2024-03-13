acd277a7e7599b0c919ba1b8c64dead8935b541a
Slava <steschenko.vyacheslav@gmail.com>
2024-03-13 10:36:20 +0100

    This web page is made to demonstrate a collection of cards

---
+++ script.js
@@ -0,0 +1,337 @@
+let collection = [
+    {
+          "name": "DUMBeenz #359",
+          "description": "DUMBeenz Season One: 420 unique & hand-crafted collectibles",
+          "image": "https://ipfs.io/ipfs/QmcmPVkTepbcJkwzdNA8uDMncww2brQdw2ewf7cVV2J4M6/359.png",
+          "explorer": "https://explorer.ergoplatform.com/en/issued-tokens?searchQuery=e99398ed3992cc9fe4965892323c205769b9cdd573dfee7a78824887e9b4d8cc",
+          "index": 359,
+          "Background": "Blue",
+          "Arms": "ThumbsUp",
+          "Body": "White",
+          "Speckles": "Multicolour2",
+          "Eyes": "TinyEyes",
+          "Hair": "PurpleSnapbackGreenWiggly",
+          "Mouth": "HalfChill",
+          "Pants": "HangingLowBlue",
+          "Shoes": "PinkSneakersGreenSocks",
+          "Intelligence": 9,
+          "Charm": 6,
+          "Beauty": 8,
+          "Bravery": 6
+    },
+    {
+          "name": "DUMBeenz #63",
+          "description": "DUMBeenz Season One: 420 unique & hand-crafted collectibles",
+          "image": "https://ipfs.io/ipfs/QmcmPVkTepbcJkwzdNA8uDMncww2brQdw2ewf7cVV2J4M6/63.png",
+          "explorer": "https://explorer.ergoplatform.com/en/issued-tokens?searchQuery=778487b86e9f4f60fded8b3f1ad389781066bf228f4cbad33c51260dd51ba904",
+          "index": 63,
+          "Background": "TieDye2",
+          "Arms": "HidyHi2",
+          "Body": "Pink",
+          "Speckles": "PinkMedium",
+          "Eyes": "YellowShades",
+          "Hair": "GreenFringe",
+          "Mouth": "GummySmile",
+          "Pants": "CheckeredOrangeOrangeSuspender",
+          "Shoes": "BlackSneakersYellowSocks",
+          "Intelligence": 9,
+          "Charm": 8,
+          "Beauty": 1,
+          "Bravery": 7
+    },
+    {
+      "name": "DUMBeenz #360",
+      "description": "DUMBeenz Season One: 420 unique & hand-crafted collectibles",
+      "image": "https://ipfs.io/ipfs/QmahNycLLAaohCczfWDxGSXpHj71vjoyqh28yGYRrD8G7c",
+      "explorer": "https://explorer.ergoplatform.com/en/issued-tokens?searchQuery=177b671726e90c33ba512fc851422d07db14293aad10d9691c7b777375b83e3b",
+      "index": 360,
+      "Background": "Pink",
+      "Arms": "HidyHi2",
+      "Body": "White",
+      "Speckles": "PinkMedium",
+      "Eyes": "YellowShades",
+      "Hair": "GreenFringe",
+      "Mouth": "GummySmile",
+      "Pants": "CheckeredOrangeOrangeSuspender",
+      "Shoes": "BlackSneakersYellowSocks",
+      "Intelligence": 1,
+      "Charm": 8,
+      "Beauty": 5,
+      "Bravery": 3
+    },
+    {
+      "name": "DUMBeenz #033",
+      "description": "DUMBeenz Season One: 420 unique & hand-crafted collectibles",
+      "image": "https://ipfs.io/ipfs/QmPYF5TxrsHhFwXfjojJnBT3quqhRWoSqj3fFp7DvbYDMT",
+      "explorer": "https://explorer.ergoplatform.com/en/issued-tokens?searchQuery=199b966de971e5eeda3adea3870a0879163da7e938ffcdbe77f851bace681f26",
+      "index": 33,
+      "Background": "Rainbow",
+      "Arms": "HidyHi2",
+      "Body": "White",
+      "Speckles": "PinkMedium",
+      "Eyes": "YellowShades",
+      "Hair": "RaibowBobbleRainbowWiggly",
+      "Mouth": "GummySmile",
+      "Pants": "CheckeredOrangeOrangeSuspender",
+      "Shoes": "BlackSneakersYellowSocks",
+      "Intelligence": 4,
+      "Charm": 8,
+      "Beauty": 4,
+      "Bravery": 4
+    },
+    {
+      "name": "DUMBeenz #015",
+      "description": "DUMBeenz Season One: 420 unique & hand-crafted collectibles",
+      "image": "https://ipfs.io/ipfs/QmcmPVkTepbcJkwzdNA8uDMncww2brQdw2ewf7cVV2J4M6/15.png",
+      "explorer": "https://explorer.ergoplatform.com/en/issued-tokens?searchQuery=c165fd93f61dfab273279c103d23652bf9d7c955d4a998b0fe9e3bdc014cbee0",
+      "index": 15,
+      "Background": "Yellow",
+      "Arms": "Regular",
+      "Body": "Blue",
+      "Speckles": "PinkMedium",
+      "Eyes": "Seedy",
+      "Hair": "GreenFringe",
+      "Mouth": "Yell",
+      "Pants": "CheckeredOrangeOrangeSuspender",
+      "Shoes": "Wellie",
+      "Intelligence": 8,
+      "Charm": 5,
+      "Beauty": 3,
+      "Bravery": 4
+    },
+    {
+      "name": "DUMBeenz #231",
+      "description": "DUMBeenz Season One: 420 unique & hand-crafted collectibles",
+      "image": "https://ipfs.io/ipfs/QmcmPVkTepbcJkwzdNA8uDMncww2brQdw2ewf7cVV2J4M6/231.png",
+      "explorer": "https://explorer.ergoplatform.com/en/issued-tokens?searchQuery=9ba52f26c83d5a91ccba4aa5b4fed625e8dc23db1bece0278c53903756b9aaf3",
+      "index": 231,
+      "Background": "Blue",
+      "Arms": "HidyHi2",
+      "Body": "Whitee",
+      "Speckles": "PinkMedium",
+      "Eyes": "Sleepy",
+      "Hair": "PinkSideburns",
+      "Mouth": "GummySmile",
+      "Pants": "HangingLowGreen",
+      "Shoes": "YellowSneakersYellowSocks",
+      "Intelligence": 9,
+      "Charm": 3,
+      "Beauty": 1,
+      "Bravery": 8
+    },
+    {
+      "name": "DUMBeenz #043",
+      "description": "DUMBeenz Season One: 420 unique & hand-crafted collectibles",
+      "image": "https://ipfs.io/ipfs/QmcmPVkTepbcJkwzdNA8uDMncww2brQdw2ewf7cVV2J4M6/43.png",
+      "explorer": "https://explorer.ergoplatform.com/en/issued-tokens?searchQuery=5c7bba54eeddaa7b9c73e6dfd6f61287d1fa8e56f75d68eaa8d331f2e85cf270",
+      "index": 43,
+      "Background": "Blue",
+      "Arms": "BreezyPits",
+      "Body": "White",
+      "Speckles": "PinkMeedium",
+      "Eyes": "YellowShades",
+      "Hair": "YellowSnapback",
+      "Mouth": "HalfChill",
+      "Pants": "CheckeredBrownOrangeSuspender",
+      "Shoes": "BlackSneakersYellowSocks",
+      "Intelligence": 3,
+      "Charm": 2,
+      "Beauty": 9,
+      "Bravery": 1
+    },
+    {
+      "name": "DUMBeenz #421",
+      "description": "DUMBeenz Season One: 420 unique & hand-crafted collectibles",
+      "image": "https://gateway.pinata.cloud/ipfs/QmfMgL9TWAL4vkevAAF5Fy3bNuTzX2XEYG33kDsPtFgknE",
+      "explorer": "https://explorer.ergoplatform.com/en/issued-tokens?searchQuery=d8506c155ba1de64f1d64be1bdcd076b8fd457130a551ec346c84b4cd9f0db3e",
+      "index": 421,
+      "Background": "NIghtmare Trip",
+      "Arms": "The simpleton's shield & sword of senselessness",
+      "Body": "Plum",
+      "Speckles": "Battle scars",
+      "Eyes": "YellowShades",
+      "Hair": "Helmet of thee illiterate",
+      "Mouth": "GummySmile",
+      "Pants": "Laundry-day tarp of the warrior",
+      "Shoes": "Boots borrowed from that strange man in the shadows",
+      "Intelligence": 0,
+      "Charm": 3,
+      "Beauty": 9,
+      "Bravery": 10
+    },
+    {
+      "name": "DUMBeenz #418",
+      "description": "DUMBeenz Season One: 420 unique & hand-crafted collectibles",
+      "image": "https://ipfs.io/ipfs/QmcmPVkTepbcJkwzdNA8uDMncww2brQdw2ewf7cVV2J4M6/418.png",
+      "explorer": "https://explorer.ergoplatform.com/en/issued-tokens?searchQuery=860ff78c21485235cfda100103d062c317db660683307b6043ce8d54a50aebc3",
+      "index": 418,
+      "Background": "Pink",
+      "Arms": "GetGroovy",
+      "Body": "Green",
+      "Speckles": "WhiteLarge",
+      "Eyes": "TinyEyes",
+      "Hair": "BrownCurls",
+      "Mouth": "GummySmile",
+      "Pants": "BlueThong",
+      "Shoes": "OrangeSocks",
+      "Intelligence": 1,
+      "Charm": 5,
+      "Beauty": 3,
+      "Bravery": 5
+    },
+    {
+      "name": "DUMBeenz #019",
+      "description": "DUMBeenz Season One: 420 unique & hand-crafted collectibles",
+      "image": "https://ipfs.io/ipfs/QmcmPVkTepbcJkwzdNA8uDMncww2brQdw2ewf7cVV2J4M6/19.png",
+      "explorer": "https://explorer.ergoplatform.com/en/issued-tokens?searchQuery=c9f67b0828538c3476c22793bed2b3900e6797e65f03c45e75a78991fed96d0f",
+      "index": 19,
+      "Background": "Green",
+      "Arms": "Regular",
+      "Body": "Green",
+      "Speckles": "PeachLarge",
+      "Eyes": "FeeelingGood",
+      "Hair": "GreenSideburns",
+      "Mouth": "Bucky",
+      "Pants": "HangingLowPink",
+      "Shoes": "OrangeSocks",
+      "Intelligence": 9,
+      "Charm": 8,
+      "Beauty": 1,
+      "Bravery": 9
+    },
+];
+
+let containerDiv = document.createElement("div");
+containerDiv.classList.add('container');
+containerDiv.style.fontFamily = "Roboto, sans-serif";
+let selectBody = document.querySelector('body');
+selectBody.appendChild(containerDiv);
+let containerForHead = document.createElement("div");
+containerForHead.classList.add('head');
+let selectContainerDiv = document.getElementsByClassName("container")[0];
+selectContainerDiv.appendChild(containerForHead);
+//making circle------------------------------------------------------------------------------------------------------------------>
+let circle = document.createElement('div');
+circle.classList.add('circle')
+//------------------------------------------------------------------------------------------------------------------------------->
+let headerOne = document.createElement('h1');
+let paragraphOne = document.createElement('p');
+let selectContainerHead = document.getElementsByClassName("head")[0];
+selectContainerHead.appendChild(circle);
+//making 2nd flexbox for head----------------------------------------------------------------------------------------------------->
+let secondHeadBox = document.createElement('div');
+secondHeadBox.classList.add('secondHeadBox');
+headerOne.appendChild(secondHeadBox);
+selectContainerHead.appendChild(secondHeadBox);
+//-------------------------------------------------------------------------------------------------------------------------------->
+secondHeadBox.appendChild(headerOne);
+secondHeadBox.appendChild(paragraphOne);
+headerOne.innerHTML = "Slava's Collection";
+headerOne.style.marginTop = "4%";
+paragraphOne.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in accumsan purus. Suspendisse iaculis consequat mi, at efficitur justo fringilla in. Proin dapibus, erat sit amet fringilla sodales, est nisi."
+
+//making card for each element----------------------------------------------------------------------------------------------------->
+for (let i = 0; i < collection.length; i++) {
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add('card');
+    selectContainerDiv.appendChild(cardDiv);
+    //creating image---------------------------------------------------------------------------------------------------------------->
+    let imgElement = document.createElement('img');
+    
+    imgElement.setAttribute('src', `${collection[i]['image']}`);
+    cardDiv.appendChild(imgElement);
+    //creating flex for buttons----------------------------------------------------------------------------------------------------->
+    let flexForButtons = document.createElement('div');
+    cardDiv.appendChild(flexForButtons);
+    //creating buttons--------------------------------------------------------------------------------------------------------------->
+    let buttonYellow = document.createElement('button');
+    buttonYellow.classList.add("buttonYellow");
+    buttonYellow.innerHTML = "buy";
+    buttonYellow.addEventListener('click', () => {
+        alert('You owe 1 kidney')
+    })
+    flexForButtons.appendChild(buttonYellow);
+    let buttonBlue = document.createElement('a');
+    buttonBlue.classList.add('buttonBlue');
+    buttonBlue.innerHTML = "explorer";
+    buttonBlue.style.height = "25px";
+    buttonBlue.setAttribute('href', `${collection[i]["explorer"]}`);
+    flexForButtons.appendChild(buttonBlue);
+    //creating title--------------------------------------------------------------------------------------------------------------->
+    let title = document.createElement('h2');
+    title.innerText = `${collection[i]["name"]}`;
+    cardDiv.appendChild(title);
+    title.style.margin = "0% 5%";
+    title.style.fontWeight = "400";
+    title.style.fontSize = "20px";
+    //creating Artist--------------------------------------------------------------------------------------------------------------->
+    let artist = document.createElement('h3');
+    artist.innerText = 'Unknown legend';
+    cardDiv.appendChild(artist);
+    artist.style.margin = "2% 5%";
+    artist.style.fontWeight = "400";
+    artist.style.fontSize = "15px";
+    artist.style.color = "grey";
+    //adding intelligence------------------------------------------------------------------------------------------------------------------>
+    let intelligence = document.createElement('p');
+    intelligence.classList.add('intelligence');
+    intelligence.innerText = `Intelligence: ${collection[i]["Intelligence"]} Charm: ${collection[i]["Charm"]} Bravery: ${collection[i]['Bravery']}`;
+    cardDiv.appendChild(intelligence);
+    intelligence.style.margin = "0% 5%";
+    intelligence.style.color = 'black';
+    //from scss file------------------------------------------------------------------------------------------------------->
+    cardDiv.style.display = 'flex';
+    cardDiv.style.flexDirection = 'column';
+    cardDiv.style.margin = '20px';
+    //cardDiv.style.width = '100%';
+    cardDiv.style.border = '1px solid red';
+    cardDiv.style.backgroundColor = 'rgb(202, 198, 198)';
+    cardDiv.style.borderRadius = '10px';
+
+    imgElement.style.height = '20em';
+    imgElement.style.borderTopLeftRadius = '10px';
+    imgElement.style.borderTopRightRadius = '10px';
+
+    buttonYellow.style.margin = '5%';
+    buttonYellow.style.width = '70px';
+    buttonYellow.style.padding = '2px 5px';
+    buttonYellow.style.height = '25px';
+    buttonYellow.style.backgroundColor = '#ffeb3b';
+    buttonYellow.style.borderRadius = '5px';
+    buttonYellow.style.border = '0px solid #ffeb3b';
+    buttonYellow.style.fontSize = '15px';
+
+    buttonBlue.style.margin = '5% 1%';
+    buttonBlue.style.width = '100px';
+    buttonBlue.style.height = '25px';
+    buttonBlue.style.padding = '5px 15px';
+    buttonBlue.style.backgroundColor = '#03a9f4';
+    buttonBlue.style.borderRadius = '5px';
+    buttonBlue.style.border = '0px solid #03a9f4';
+    buttonBlue.style.textDecoration = 'none';
+    buttonBlue.style.color = 'black';
+    buttonBlue.style.fontSize = '15px';
+}
+//from scss file -------------------------------------------------------------------------------------------------------------->
+selectBody.style.display = 'flex';
+selectBody.style.backgroundColor = 'palegreen';
+
+containerDiv.style.display = 'flex';
+containerDiv.style.flexWrap = 'wrap';
+containerDiv.style.alignItems = 'center';
+containerDiv.style.padding = '20px';
+containerDiv.style.color = 'darkblue';
+
+containerForHead.style.display = 'flex';
+containerForHead.style.flexDirection = 'row';
+containerForHead.style.alignItems = 'center';
+
+secondHeadBox.style.width = '70%';
+secondHeadBox.style.marginLeft = '5%';
+
+circle.style.width = '50px';
+circle.style.height = '50px';
+circle.style.borderRadius = '50%';
+circle.style.backgroundColor = 'darkblue';
+circle.style.marginLeft = '5%';
+//----------------------------------------------------------------------------------------------------------------------------->
